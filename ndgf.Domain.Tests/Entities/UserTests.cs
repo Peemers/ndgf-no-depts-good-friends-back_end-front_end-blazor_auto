b5417a96a5da6eabf04fefde8faf533e61cfeea6
Math Peeters <pmathieu.peeters@gmail.com>
2026-07-28 13:58:31 +0200

    feat(domain): add User entity with validation rules and unit tests

---
+++ ndgf.Domain.Tests/Entities/UserTests.cs
@@ -0,0 +1,175 @@
+﻿using ndgf.Domain.Entities;
+using ndgf.Domain.Exceptions;
+
+namespace ndgf.Domain.Tests.Entities;
+
+public class UserTests
+{
+  [Fact]
+  public void Create_WithValidData_ShouldCreateUser()
+  {
+    //Arrange
+    var email = "test@test.be";
+    var passwordHash = "test123";
+    var pseudo = "Toto";
+    var firstName = "Jean";
+    var lastName = "Dupont";
+    
+    //Act
+    
+    var user = User.Create(email, passwordHash, pseudo, lastName, firstName);
+    
+    //Assert
+    
+    Assert.Equal(email, user.Email);
+    Assert.Equal(passwordHash, user.PasswordHash);
+    Assert.Equal(pseudo, user.Pseudo);
+    Assert.Equal(firstName, user.FirstName);
+    Assert.Equal(lastName, user.LastName);
+    Assert.NotEqual(Guid.Empty, user.Id);
+  }
+
+  [Theory]
+  [InlineData("")]
+  [InlineData(" ")]
+  [InlineData("emailinvalide")]
+  [InlineData("email@")]
+  [InlineData("@test.com")]
+  public void Create_WithInvalidEmail_ShouldThrowDomainException(string invalidEmail)
+  {
+    //Arrange
+    
+    var passwordHash = "test123";
+    var pseudo = "Toto";
+    var firstName = "Jean";
+    var lastName = "Dupont";
+    
+    //Act & Assert
+    
+    Assert.Throws<DomainException>(() => User.Create(invalidEmail, passwordHash, pseudo, lastName, firstName));
+  }
+
+  [Theory]
+  [InlineData("")]
+  [InlineData(" ")]
+  public void Create_WithInvalidPasswordHash_ShouldThrowDomainException(string invalidPasswordHash)
+  {
+    var pseudo = "Toto";
+    var firstName = "Jean";
+    var lastName = "Dupont";
+    var email = "test@test.be";
+    
+    Assert.Throws<DomainException>(() => User.Create(email, invalidPasswordHash, pseudo, lastName, firstName));
+  }
+
+  [Theory]
+  [InlineData("")]
+  [InlineData(" ")]
+  public void Create_WithInvalidPseudo_ShouldThrowDomainException(string invalidPseudo)
+  {
+    var firstName = "Jean";
+    var lastName = "Dupont";
+    var email = "test@test.be";
+    var passwordHash = "test123";
+    
+    Assert.Throws<DomainException>(() => User.Create(email, passwordHash, invalidPseudo, lastName, firstName));
+  }
+  
+  [Fact]
+  public void Create_WithTooLongPseudo_ShouldThrowDomainException()
+  {
+    var email = "test@test.be";
+    var passwordHash = "test123";
+    var tooLongPseudo = new string('a', 31);
+    var firstName = "Jean";
+    var lastName = "Dupont";
+    
+    Assert.Throws<DomainException>(() => User.Create(email, passwordHash, tooLongPseudo, lastName, firstName));
+  }
+
+  [Fact]
+  public void Create_WithTooShortPseudo_ShouldThrowDomainException()
+  {
+    var email = "test@test.be";
+    var passwordHash = "test123";
+    var tooShortPseudo = new string('a', 1);
+    var firstName = "Jean";
+    var lastName = "Dupont";
+    
+    Assert.Throws<DomainException>(() => User.Create(email, passwordHash, tooShortPseudo, lastName, firstName));
+  }
+
+  [Theory]
+  [InlineData("")]
+  [InlineData(" ")]
+  public void Create_WithInvalidFirstName_ShouldThrowDomainException(string invalidFirstName)
+  {
+    var email = "test@test.be";
+    var passwordHash = "test123";
+    var pseudo = "Toto";
+    var lastName = "Dupont";
+    
+    Assert.Throws<DomainException>(() => User.Create(email, passwordHash, pseudo, lastName, invalidFirstName));
+  }
+
+  [Fact]
+  public void Create_WithTooLongFirstName_ShouldThrowDomainException()
+  {
+    var email = "test@test.be";
+    var passwordHash = "test123";
+    var pseudo = "Toto";
+    var lastName = "Dupont";
+    var toLongFirstName = new string('a', 31);
+    
+    Assert.Throws<DomainException>(() => User.Create(email, passwordHash, pseudo, lastName, toLongFirstName));
+  }
+  
+  [Fact]
+  public void Create_WithTooShortFirstName_ShouldThrowDomainException()
+  {
+    var email = "test@test.be";
+    var passwordHash = "test123";
+    var pseudo = "Toto";
+    var lastName = "Dupont";
+    var toShortFirstName = new string('a', 1);
+    
+    Assert.Throws<DomainException>(() => User.Create(email, passwordHash, pseudo, lastName, toShortFirstName));
+  }
+  
+  [Theory]
+  [InlineData("")]
+  [InlineData(" ")]
+  public void Create_WithInvalidLastName_ShouldThrowDomainException(string invalidLastName)
+  {
+    var email = "test@test.be";
+    var passwordHash = "test123";
+    var pseudo = "Toto";
+    var firstName = "Dupont";
+    
+    Assert.Throws<DomainException>(() => User.Create(email, passwordHash, pseudo, invalidLastName, firstName));
+  }
+
+  [Fact]
+  public void Create_WithTooLongLastName_ShouldThrowDomainException()
+  {
+    var email = "test@test.be";
+    var passwordHash = "test123";
+    var pseudo = "Toto";
+    var firstName = "Dupont";
+    var toLongLastName = new string('a', 31);
+    
+    Assert.Throws<DomainException>(() => User.Create(email, passwordHash, pseudo, toLongLastName, firstName ));
+  }
+  
+  [Fact]
+  public void Create_WithTooShortLastName_ShouldThrowDomainException()
+  {
+    var email = "test@test.be";
+    var passwordHash = "test123";
+    var pseudo = "Toto";
+    var firstName = "Jean";
+    var toShortLastName = new string('a', 1);
+    
+    Assert.Throws<DomainException>(() => User.Create(email, passwordHash, pseudo, toShortLastName, firstName));
+  }
+}
---
+++ ndgf.Domain/Entities/User.cs
@@ -0,0 +1,102 @@
+﻿using System.Text.RegularExpressions;
+using ndgf.Domain.Exceptions;
+
+namespace ndgf.Domain.Entities;
+
+//Partial pour autoriser le GenerateRegex (source generator) d'écrire du code optimisé pour moi dans les fichiers de build et non exécuté au runtime (gain de performance)
+
+public sealed partial class User
+{
+  private const int PseudoMaxLength = 30;
+  private const int FirstNameLastNameMaxLength = 30;
+  private const int PseudoMinLength = 3;
+  private const int FirstNameLastNameMinLength = 2;
+
+  public Guid Id { get; private set; }
+  public string Email { get; private set; } = null!;
+  public string PasswordHash { get; private set; } = null!;
+  public string Pseudo { get; private set; } = null!;
+  public string FirstName { get; private set; } = null!;
+  public string LastName { get; private set; } = null!;
+
+  private User()
+  {
+  }
+
+  private User(Guid id, string email, string passwordHash, string pseudo, string lastName, string firstName)
+  {
+    Id = id;
+    Email = email;
+    PasswordHash = passwordHash;
+    Pseudo = pseudo;
+    FirstName = firstName;
+    LastName = lastName;
+  }
+
+  public static User Create(string email, string passwordHash, string pseudo, string lastName, string firstName)
+  {
+    if (string.IsNullOrWhiteSpace(email))
+    {
+      throw new DomainException("L'email est obligatoire.");
+    }
+
+    if (!EmailRegex().IsMatch(email))
+    {
+      throw new DomainException("Le format de l'émail est invalide");
+    }
+
+    if (string.IsNullOrWhiteSpace(passwordHash))
+    {
+      throw new DomainException("Le mot de passe est obligatoire.");
+    }
+
+    if (string.IsNullOrWhiteSpace(pseudo))
+    {
+      throw new DomainException("Le pseudo est obligatoire.");
+    }
+
+    if (pseudo.Length > PseudoMaxLength)
+    {
+      throw new DomainException($"Le pseudo ne peut pas dépasser {PseudoMaxLength} caractères.");
+    }
+
+    if (pseudo.Length < PseudoMinLength)
+    {
+      throw new DomainException($"Le pseudo ne peut comporter moins de {PseudoMinLength} caractères");
+    }
+
+    if (string.IsNullOrWhiteSpace(lastName))
+    {
+      throw new DomainException("Le nom est obligatoire.");
+    }
+
+    if (lastName.Length > FirstNameLastNameMaxLength)
+    {
+      throw new DomainException($"Le nom ne peut pas dépasser {FirstNameLastNameMaxLength} caractères.");
+    }
+
+    if (lastName.Length < FirstNameLastNameMinLength)
+    {
+      throw new DomainException($"Le nom ne peut pas comporter moins de {FirstNameLastNameMinLength}");
+    }
+
+    if (string.IsNullOrWhiteSpace(firstName))
+    {
+      throw new DomainException("Le prénom est obligatoire.");
+    }
+
+    if (firstName.Length > FirstNameLastNameMaxLength)
+    {
+      throw new DomainException($"Le prénom ne peut pas comporter plus de {FirstNameLastNameMaxLength} caractères.");
+    }
+
+    if (firstName.Length < FirstNameLastNameMinLength)
+    {
+      throw new DomainException($"Le prénom ne peut pas comporter moins de {FirstNameLastNameMinLength} caractères");
+    }
+
+    return new User(Guid.NewGuid(), email, passwordHash, pseudo, lastName, firstName);
+  }
+  [GeneratedRegex(@"^[^@\s]+@[^@\s]+\.[^@\s]+$")]
+  private static partial Regex EmailRegex();
+}
