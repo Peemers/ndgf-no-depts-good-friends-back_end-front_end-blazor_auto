3fbbc664eaffe311710aed9279f87dd4eeb185ca
Math Peeters <pmathieu.peeters@gmail.com>
2026-07-29 21:08:43 +0200

    feat(web):create blazor project, add registration form with Tailwind and API integration

---
+++ ndgf.Web/ndgf.Web/Services/User/UserApiClient.cs
@@ -0,0 +1,11 @@
+﻿using ndgf.Web.Models.User;
+
+namespace ndgf.Web.Services.User;
+
+public class UserApiClient(HttpClient httpClient)
+{
+  public async Task<HttpResponseMessage> RegisterAsync(RegisterUserModel model)
+  {
+    return await httpClient.PostAsJsonAsync("/api/users/register", model);
+  }
+}
