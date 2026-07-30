bc0b6e4e884ba7af9e96ff39df3bb67fe212d6a6
Math Peeters <pmathieu.peeters@gmail.com>
2026-07-30 20:10:57 +0200

    feat(api): add login endpoint with DTOs, mapper and OpenAPI documentation

---
+++ ndgf.Api/Dtos/User/Response/LoginUserResponseDto.cs
@@ -0,0 +1,9 @@
+﻿namespace ndgf.Api.Dtos.User.Response;
+
+public record LoginUserResponseDto
+{
+  public  Guid Id { get; init; }
+  public required string Email { get; init; }
+  public required string AccessToken { get; init; }
+  public required string RefreshToken { get; init; }
+}
