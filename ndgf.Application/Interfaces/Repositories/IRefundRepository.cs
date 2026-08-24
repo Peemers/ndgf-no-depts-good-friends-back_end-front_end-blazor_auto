639ca4b907ecf7394a361865a2b40627393eb053
Math Peeters <pmathieu.peeters@gmail.com>
2026-08-24 13:30:06 +0200

    feat(infrastructure): add Refund repository, configuration and migration

---
+++ ndgf.Application/Interfaces/Repositories/IRefundRepository.cs
@@ -0,0 +1,8 @@
+﻿using ndgf.Domain.Entities;
+
+namespace ndgf.Application.Interfaces.Repositories;
+
+public interface IRefundRepository
+{
+  Task<Refund> AddAsync(Refund refund);
+}
---
+++ ndgf.Infrastructure/Persistence/Repositories/RefundRepository.cs
@@ -0,0 +1,14 @@
+﻿using ndgf.Application.Interfaces.Repositories;
+using ndgf.Domain.Entities;
+
+namespace ndgf.Infrastructure.Persistence.Repositories;
+
+public class RefundRepository(NdgfDbContext context) : IRefundRepository
+{
+  public async Task<Refund> AddAsync(Refund refund)
+  {
+    await context.Refunds.AddAsync(refund);
+    await context.SaveChangesAsync();
+    return refund;
+  }
+}
